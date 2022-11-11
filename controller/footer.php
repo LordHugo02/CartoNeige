e9e5a7633d689bdfebb335cbab9cfb45e16b83e2
LordHugo02 <hugoflinois@hotmail.fr>
2022-11-11 19:54:04 +0100

    Passage des clés API dans un fichier de config.js

--- controller/footer.php
+++ controller/footer.php
@@ -2,7 +2,8 @@
 
     
     //import js fontawesome
-
+    
+    echo '<script type="text/javascript" src="' . $pub_url . 'config/config.js"></script>';
     echo '<script type="text/javascript" src="' . $pub_url . 'vendor/font-awesome/js/all.js"></script>';
     echo '<script type="text/javascript" src="' . $pub_url . 'vendor/leaflet/leaflet.js"></script>';
     echo '<script type="text/javascript" src="' . $pub_url . 'src/js/map.js"></script>';
