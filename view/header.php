b4b6edd4f225b770753d3704d13a089335449743
carcassonne1104 <carcassonne1104@gmail.com>
2023-05-09 15:18:42 +0200

    Modifications header

--- view/header.php
+++ view/header.php
@@ -5,6 +5,9 @@
     <div id="centre">
         <p id="texte">Site de consultation météorologique pour randonneurs 🦊 <i class="fa-solid fa-shield-halved"></i></p>
     </div>
+    <div id="liens">
+        <a class="lien" href="https://www.theia-land.fr/product/neige/">Lien vers le site de Theia</a>
+    </div>
     <div id="credit">
         <p id="noms">Développé par FLINOIS Hugo et DIJOUX Quentin</p>
     </div>
